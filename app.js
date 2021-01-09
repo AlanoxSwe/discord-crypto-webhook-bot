37940edb613ad5ec345d3934c612c93e0b7d667b
AlanoxSwe <61416243+AlanoxSwe@users.noreply.github.com>
2021-01-09 14:58:42 +0100

    project offload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {
+  client, sendNewCoin, sendPercentPrice, sendPeriodicPrice, sendPrepumpPrice
+} = require('./bot');
+require('dotenv').config();
+
+const { DISCORD_KEY, WEBHOOK_URL } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+client.login(DISCORD_KEY);
+
+app.post(`/${WEBHOOK_URL}`, (req, res) => {
+  const { type, currency, message, window } = req.body;
+  switch(type) {
+    case "percent_price":
+      if(window.includes("1 minutes")) {
+        sendPrepumpPrice(currency, message);
+      } else {
+        sendPercentPrice(currency, message);
+      }
+      res.status(200).end();
+      break;
+    case "new_coin":
+      sendNewCoin(currency, message);
+      res.status(200).end();
+      break;
+    case "periodic_price":
+      sendPeriodicPrice(message);
+      res.status(200).end();
+      break;
+    default:
+      res.status(500).end();
+      break;
+  }
+});
+
+module.exports = app;
---
+++ bot.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+const Discord = require('discord.js');
+const Axios = require('axios');
+require('dotenv').config();
+const { CMC_KEY } = process.env;
+
+const client = new Discord.Client();
+
+// Helper Methods
+
+const getLogo = async (currency) => {
+  const { data } = await Axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=${CMC_KEY}&symbol=${currency}`);
+  return data.data[currency].logo;
+}
+
+const getPercentFromMessage = (msg) => {
+  const percentRegex = /[0-9]+(.[0-9])+/i;
+  return msg.match(percentRegex)[0];
+}
+
+// Bot Methods
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const sendPercentPrice = async (currency, msg) => {
+  const logo = await getLogo(currency);
+  const channel = client.channels.cache.find((chnl) => chnl.name === "📢crypto-alerts");
+  const embed = new Discord.MessageEmbed()
+      .setTitle(`+${getPercentFromMessage(msg)}%`)
+      .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+      .setColor(0x00ff00)
+      .setDescription(msg);
+  channel.send(embed);
+};
+
+const sendPrepumpPrice = async (currency, msg) => {
+  const logo = await getLogo(currency);
+  const channel = client.channels.cache.find((chnl) => chnl.name === "📢crypto-alerts");
+  const embed = new Discord.MessageEmbed()
+      .setTitle(`POSSIBLE PRE-PUMP: +${getPercentFromMessage(msg)}%`)
+      .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+      .setColor(0xff0000)
+      .setDescription(msg);
+  channel.send("@here Possible pre-pump detected:");
+  channel.send(embed);
+};
+
+const sendNewCoin = async (currency, msg) => {
+  const logo = await getLogo(currency);
+  const channel = client.channels.cache.find((chnl) => chnl.name === "📢crypto-alerts");
+  const embed = new Discord.MessageEmbed()
+      .setTitle("Newly listed!")
+      .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+      .setColor(0xffff00)
+      .setDescription(msg);
+  channel.send(embed);
+};
+
+const sendPeriodicPrice = (msg) => {
+  const channel = client.channels.cache.find((chnl) => chnl.name === "bot-test");
+  const embed = new Discord.MessageEmbed()
+      .setTitle(msg)
+      .setColor(0xff9900)
+  channel.send(embed);
+};
+
+module.exports = {
+  client, sendNewCoin, sendPercentPrice, sendPeriodicPrice, sendPrepumpPrice
+};
